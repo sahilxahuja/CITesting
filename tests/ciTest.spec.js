625a2cc685379d276e8e6e871e6d8d7493ab3800
sahil <sahilahuja1@live.in>
2026-01-16 01:40:13 +0530

    Second Commit

---
+++ tests/ciTest.spec.js
@@ -0,0 +1,11 @@
+const{test, expect, chromium} = require('@playwright/test')
+
+test('Check GoogleHomePage', async({browser}) => {
+
+  //const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  
+  await page.goto('https://www.google.com'); 
+
+})
