5fd28c5e9e076fb2d2cf59cc6713b1083f834dae
sahil <sahilahuja1@live.in>
2026-01-20 23:36:30 +0530

    Added table1

---
+++ tests/table1.spec.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@playwright/test')
+
+test('writing table elements', async ({ page }) => {
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+
+    const table = await page.locator("//table[@name ='BookTable']")
+    const cols = await table.locator('tbody tr th')
+
+    console.log(await cols.count());
+    const rows = await table.locator("tbody tr")
+    const colcount = await cols.count();
+    console.log(await rows.count());
+    const rcount = await rows.count();
+
+    for (let i = 0; i < rcount; i++) {
+        const curr_row = rows.nth(i);
+        const curr_cell = curr_row.locator('td');
+
+        for (let j = 0; j < await curr_cell.count(); j++) {
+
+            const tdata = await curr_cell.nth(j).textContent();
+            console.log(tdata)
+        }
+
+
+    }
+});
